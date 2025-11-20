2cbf3f0c0057f81121c6fa90db352b8d37d4da53
Adrien Laval <2ndlife@MacBook-Pro-de-Adrien.local>
2025-11-20 17:26:35 +0100

    enlever print

--- src/naive/build.py
+++ src/naive/build.py
@@ -2,7 +2,6 @@ def dbg_builder(fasta_file,color, k,dbg):
     with open(fasta_file,"r") as f:
         for line in f:
             line = line.strip()
-            print(line)
             if not line.startswith('>'):
                 for i in range(0,len(line)-k):
                     kmer = line[i:i+k]
