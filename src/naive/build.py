a8e4d28fa1c39d0d2cea470869ad6a0de51cf493
Adrien Laval <2ndlife@MacBook-Pro-de-Adrien.local>
2025-11-20 17:59:40 +0100

    fct loop

--- src/naive/build.py
+++ src/naive/build.py
@@ -1,4 +1,9 @@
 def dbg_builder(fasta_file,color, k,dbg):
+    """
+    Creer le dictionnaire contenant le graphe de De Brujns coloré
+    fasta_file = pwd du fichier fasta,color indice du fichier dans sa liste, k taille des kmers,dbg dictionaire du graphe
+    renvoi le dictionaire mis à jours
+    """
     with open(fasta_file,"r") as f:
         for line in f:
             line = line.strip()
@@ -7,7 +12,20 @@ def dbg_builder(fasta_file,color, k,dbg):
                     kmer = line[i:i+k]
                     if kmer not in dbg.keys():
                         dbg[kmer] = [color]
-                    else:
+                    elif color not in dbg[kmer]:
                         dbg[kmer].append(color)
     return dbg
 
+
+
+def loop(file_list,k):
+    dbg = {}
+    i = 0 
+    with open(file_list,"r") as fl:
+        for file in fl:
+            file =file.strip()
+            dbg = dbg_builder(file,i,k,dbg)
+            i+=1
+    return dbg
+
+print(loop("/Users/2ndlife/Documents/Rennes M2/ALG/Projet_alg/G.txt",17))
