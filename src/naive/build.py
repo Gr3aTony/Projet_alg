f84e82f21e037a52468214ef056c8bf22a5d82f8
Adrien Laval <2ndlife@MacBook-Pro-de-Adrien.local>
2025-11-20 17:17:55 +0100

    premier build

--- src/naive/build.py
+++ src/naive/build.py
@@ -1,1 +1,13 @@
-
+def dbg_builder(fasta_file,color, k,dbg):
+    with open(fasta_file,"r") as f:
+        for line in f:
+            line = line.strip()
+            print(line)
+            if not line.startswith('>'):
+                for i in range(0,len(line)-k):
+                    kmer = line[i:i+k]
+                    if kmer not in dbg.keys():
+                        dbg[kmer] = [color]
+                    else:
+                        dbg[kmer].append(color)
+    return dbg
