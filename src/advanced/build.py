6fec9b82a0e4017dac2616d6b8e48a6e1f762b78
Gr3aTony <74353921+Gr3aTony@users.noreply.github.com>
2025-12-17 18:38:42 +0100

    Adjust sliding window range for k-mer extraction
    Fix sliding window range to include last k-mer.

--- src/advanced/build.py
+++ src/advanced/build.py
@@ -43,7 +43,7 @@ def dbg_builder(fasta_file: str, color: int, k: int, dbg: dict):
         seq = str(record.seq)
 
         # Sliding window extraction of k-mers
-        for i in range(0, len(seq) - k):
+        for i in range(0, len(seq) - k + 1):
             kmer = seq[i: i + k]
 
             # Insert the k-mer and associate it with the current color
